b7c4e92fd7598a8b0ba4202e9414deb7c7811fb8
Hack Reactor Student <student@hackreactor.com>
2017-04-07 20:23:51 -0700

    GOT MESSAGES FROM SERVER TO LOAD AND ROOMNAMES

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,5 @@
 // YOUR CODE HERE:
-
+console.log(window)
 
 var app = {
 	server: "http://parse.sfm8.hackreactor.com/chatterbox/classes/messages",
@@ -22,7 +22,9 @@ app.send = function(message) {
 		url:this.server,
 		type: 'POST',
 		data: JSON.stringify(message),
-  		contentType: 'application/json', success: function (data) { console.log('chatterbox: Message sent');
+  		contentType: 'application/json', 
+  		success: function (data) { 
+  			console.log('Message sent successfully')
  		 },
   			error: function (data) {
     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -39,7 +41,23 @@ app.fetch = function() {
 	  type: 'GET',
 	  contentType: 'application/json',
 	  success: function(data) {
-	  	console.log(data);
+	  	var data = data.results;
+	  	var renderobj = {}
+	  	var room;
+	  	for(var i = 0; i < data.length; i++){
+	  		console.log(data);
+	  		app.renderMessage(data[i]);
+
+			room = data[i].roomname
+
+			renderobj[room] = room;
+
+	  	}
+
+	  	for ( var key in renderobj) {
+
+	  		app.renderRoom(renderobj[key]);
+	  	}
 	  }
 
 	  // data: 
@@ -53,33 +71,43 @@ app.clearMessages = function() {
 }
 
 app.renderMessage = function(message) {
-	$('#chats').append('<div class="username">'+message.username+'</div>');
-	$('#main').append('<div class="username">'+message.username+'</div>')
-
+	$('#chats').append('<div class="message"><div class="username">'+message.username+'</div><div class="chat">'+message.text+'</div></div>');
+	// $('#main').append('<div class="username">'+message.username+'</div>')
 }
 
 app.renderRoom = function(string) {
 
-	$('#roomSelect').append('<div class= '+string+'>'+string+'</div>')
+	$('#roomSelect').append('<ul><a href="#" class="room">'+string+'</a></ul>');
 
 }
 
 app.handleUsernameClick =function() {
-
-
-
 }
 
 
 app.handleSubmit = function() {
+	// find your name
+	//set your name into some object 
+	//grab text and 
+	var obj = {
+		username: window.location.search,
+		text: $('#textFeild').val()
+	}
 
-
+	app.send(obj);
 }
 
 
 
+// var search = function() {
+// 	var messagesArray = app.fetch();
+// 	for (var i = 0; i < messagesArray.results.length; i++){
 
+// 		app.renderMessage(i);
+// 	}
+// }
 
+// search();
 
 
 
