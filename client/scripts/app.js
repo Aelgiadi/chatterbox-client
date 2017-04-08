a84599a08900766e6be61f25e645f78f73479033
Hack Reactor Student <student@hackreactor.com>
2017-04-07 17:25:13 -0700

    COMPLETED CHATTERBOX TEST

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,85 @@
 // YOUR CODE HERE:
+
+
+var app = {
+	server: "http://parse.sfm8.hackreactor.com/chatterbox/classes/messages",
+}; 
+
+
+app.init = function() { 
+
+$('.username').on('click', app.handleUsernameClick());
+$('#send .submit').on('submit', app.handleSubmit());
+
+}
+
+
+app.send = function(message) {
+
+
+	$.ajax({ 
+
+		url:this.server,
+		type: 'POST',
+		data: JSON.stringify(message),
+  		contentType: 'application/json', success: function (data) { console.log('chatterbox: Message sent');
+ 		 },
+  			error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    		console.error('chatterbox: Failed to send message', data);
+ 		 }
+ 		})
+
+	
+}
+
+app.fetch = function() {
+	$.ajax({
+	  url: this.server, 
+	  type: 'GET',
+	  contentType: 'application/json',
+	  success: function(data) {
+	  	console.log(data);
+	  }
+
+	  // data: 
+	  // success: success,
+	  // dataType: dataType
+	});
+}
+
+app.clearMessages = function() {	
+	$('#chats').empty();
+}
+
+app.renderMessage = function(message) {
+	$('#chats').append('<div class="username">'+message.username+'</div>');
+	$('#main').append('<div class="username">'+message.username+'</div>')
+
+}
+
+app.renderRoom = function(string) {
+
+	$('#roomSelect').append('<div class= '+string+'>'+string+'</div>')
+
+}
+
+app.handleUsernameClick =function() {
+
+
+
+}
+
+
+app.handleSubmit = function() {
+
+
+}
+
+
+
+
+
+
+
+
